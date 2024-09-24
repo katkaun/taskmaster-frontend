694af63e18def4c79b2985491661271d0b017f20
OlssonMartin <martin1862@hotmail.com>
2024-09-24 16:52:35 +0200

    Placeholders

---
+++ src/stories/components/Profile/Profile.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Profile from "./Profile";
+
+export default {
+  title: "taskmaster/components/Profile",
+  component: Profile,
+};
+
+export const Default = () => <Profile />;
