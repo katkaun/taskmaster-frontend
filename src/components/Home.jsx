ebb8e6b9c8f59b16139cd0c0243002de55e51a5a
Katariina Kaunisto <katariina.kaunisto@hotmail.se>
2024-10-06 11:14:47 +0200

    fixed onLogin in Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
 import { Register, Login } from "@katkaun/taskmaster-components";
 import styles from "../styles/Home.module.css";
 
+const handleLogin = (username, password) => {
+  console.log("Login attempted with:", username, password);
+};
+
 const Home = () => {
   return (
     <div className={styles.container}>
-      <Login />
+      <Login onLogin={handleLogin} /> 
       <Register />
     </div>
   );
